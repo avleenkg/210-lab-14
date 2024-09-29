ba2207717507207de56a857136e0d89c54bf98b8
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-09-29 15:52:22 -0700

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Color {
+    private:
+    int rhues;
+    int ghues;
+    int bhues;
 
+    public:
+    int getred()       { return rhues; }
+    int getblue()      { return bhues; }
+    int getgreen()     { return ghues; }
+    void    
+
+};
 
 int main() {
 
