58a614fc4d18023cb04d94b5de40de6491c3a378
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-09-29 16:12:56 -0700

    commit 4

--- main.cpp
+++ main.cpp
@@ -28,6 +28,22 @@ class Color {
 };
 
 int main() {
+    Color c1;
+    c1.setred(255);
+    c1.setblue(0);
+    c1.setblue(0);
+    c1.print();
+
+    Color c2;
+    c2.setred(115);
+    c2.setblue(20);
+    c2.setgreen(255);
+    c2.print();
     
+    Color c3;
+    c3.setred(0);
+    c3.setblue(255);
+    c3.setgreen(100);
+    c3.print();
 
 }
