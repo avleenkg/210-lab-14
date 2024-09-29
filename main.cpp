23bb2403c18847460024a85b3add4ab8fa550fe1
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-09-29 16:05:48 -0700

    commit 3

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
+//Avleen Gill - 210 Lab 14
+
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+const int W = 15;
 
 class Color {
     private:
     int rhues;
-    int ghues;
     int bhues;
+    int ghues;
 
     public:
-    int getred()       { return rhues; }
-    int getblue()      { return bhues; }
-    int getgreen()     { return ghues; }
-    void    
+    int getred() const      { return rhues; }
+    int getblue() const     { return bhues; }
+    int getgreen() const    { return ghues; }
+    void setred(int r)      { rhues = r; }   
+    void setblue(int b)      { bhues = b; }
+    void setgreen(int g)      { ghues = g; }
 
+    void print() {
+        cout << setw(15) << "Red hues: " << rhues << endl;
+        cout << setw(15) << "Blue hues: " << bhues << endl;
+        cout << setw(15) << "Green hues: " << ghues << endl;
+    }
 };
 
 int main() {
+    
 
 }
